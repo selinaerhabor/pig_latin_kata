860329bd5d00b3acd6fb86c6212e44354961ce59
Selina Erhabor <selinaerhabor@gmail.com>
2019-10-22 09:56:01 +0100

    Added attempt to exercise_2

--- exercise_2.rb
+++ exercise_2.rb
@@ -8,8 +8,35 @@
 # Still assume the words are lowercase.
 
 def pig_latin(word)
-
+  vowels = ["a", "e", "i", "o", "u"]
+  consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+  if vowels.include?(word[0])
+    "#{word}way"
+  elsif vowels.include?(word[1])
+    a = "#{word}".slice!(1..-1) + word[0]
+    "#{a}ay"
+  elsif vowels.include?(word[3])
+    a = "#{word}".slice!(3..-1) + word[0] + word[1] + word[2]
+    "#{a}ay"
+  elsif vowels.include?(word[4])
+    a = "#{word}".slice!(4..-1) + word[0] + word[1] + word[2] + word[3]
+    "#{a}ay"
+  elsif consonants.include?(word[0])
+    a = "#{word}".slice!(1..-1) + word[0]
+    "#{a}ay"
+  consonants.include?(word[0] && word[1])
+    a = "#{word}".slice!(2..-1) + word[0] + word[1]
+    "#{a}ay"
+  elsif consonants.include?(word[0] && word[1] && word[2])
+    a = "#{word}".slice!(3..-1) + word[0] + word[1] + word[2]
+    "#{a}ay"
+  else
+    a = "#{word}".slice!(1..-1) + word[0]
+    "#{a}ay"
+  end
 end
+
+pig_latin("syllab")
 
 ## Tests:
 
